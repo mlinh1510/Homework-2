5d21591ac524463aec4d64c4fc6460a864bce865
mlinh1510 <98192872+mlinh1510@users.noreply.github.com>
2022-04-09 19:27:32 -0500

    Add files via upload

---
+++ part b.py
@@ -0,0 +1,20 @@
+def find(read_date):
+    months = {"January": 1, "February": 2,  "March": 3, "April": 4, "May": 5, "June": 6, "July": 7,
+              "August": 8, "September": 9, "October": 10, "November": 11, "December": 12}
+
+    try:
+        year = read_date.split(",")[-1].strip()
+        month = read_date.split(",")[0].split()[0]
+        day = read_date.split(",")[0].split()[-1]
+        mm = months[month]
+        int(year)
+        int(day)
+        return str(mm) + "/" + day + "/" + year
+    except:
+        return""
+
+with open("inputDates.txt") as f:
+    for j in f.readline():
+        if j.strip() != "-1":
+            print(find(j.strip()))
+
